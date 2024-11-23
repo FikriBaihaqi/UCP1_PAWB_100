ddd8db32a065c86dbf7598235a66ef19471934b8
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2024-11-23 23:07:00 +0700

    membuat endpoint dari get post put dan delete buat bibit

--- routes/bibit.js
+++ routes/bibit.js
@@ -19,4 +19,46 @@ const bibit = [
         usia: 15,
     }, 
 ]; 
-
+router.get("/", (req, res) => { 
+    res.send(bibit); 
+}); 
+ 
+router.post('/', (req, res) => { 
+    const newbibit = { 
+        id: bibit.length + 1, 
+        namabibit: req.body.namabibit, 
+        jenis: req.body.jenis, 
+        usia: req.body.usia
+    }; 
+    bibit.push(newbibit); 
+    res.status(201).json(newbibit); 
+}); 
+ 
+router.put('/:id', (req, res) => { 
+    const bibitIndex = bibit.findIndex(t => t.id === parseInt(req.params.id)); 
+    if (!bibitIndex === -1) 
+        return res.status(404).json({ message: 'Tugas tidak ditemukan' }); 
+ 
+    bibit[bibitIndex] = { 
+        ...bibit[bibitIndex], 
+        namabibit: req.body.namabibit || bibit[bibitIndex].namabibit, 
+        jenis: req.body.jenis || bibit[bibitIndex].jenis, 
+        usia: req.body.usia || bibit[bibitIndex].usia, 
+    }; 
+    
+ 
+    res.status(200).json({ 
+        message: `Tugas dengan ID '${req.params.id}' telah diperbarui`, 
+        updateBibit: bibit[bibitIndex], 
+    }); 
+}); 
+ 
+router.delete('/:id', (req, res) => { 
+    const bibitIndex = bibit.findIndex(t => t.id === parseInt(req.params.id)); 
+    if (bibitIndex === -1) return res.status(404).json({ message: 'Tugas tidak ditemukan' }); 
+ 
+    const deleteBibit = bibit.splice(bibitIndex, 1)[0]; // Menghapus dan menyimpan todo yang dihapus 
+    res.status(200).json({ message: `Tugas '${deleteBibit.namabibit}' telah dihapus` }); 
+ 
+}); 
+export default router;
